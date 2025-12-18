51e727f955a4afb33d2c8539b09ad4d6ad2a0289
jinal.patel <jinal.patel@aixtor.com>
2025-12-18 14:37:28 +0530

    committing changes for notification

--- modules/ax-hrms-work-from-home-web/src/main/java/com/ax/hrms/work/from/home/web/employee/notification/SendNotificationToUserHandler.java
+++ modules/ax-hrms-work-from-home-web/src/main/java/com/ax/hrms/work/from/home/web/employee/notification/SendNotificationToUserHandler.java
@@ -1,12 +1,13 @@
-package com.ax.hrms.leave.management.manager.web.notification;
+package com.ax.hrms.work.from.home.web.employee.notification;
 
-import com.ax.hrms.leave.management.web.constants.AxHrmsLeaveManagementSystemWebPortletKeys;
+import com.ax.hrms.work.from.home.web.constants.AxHrmsWorkFromHomePortletKeys;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.UserNotificationEvent;
 import com.liferay.portal.kernel.notifications.BaseUserNotificationHandler;
 import com.liferay.portal.kernel.notifications.UserNotificationHandler;
 import com.liferay.portal.kernel.service.ServiceContext;
+
 import org.osgi.service.component.annotations.Component;
 
 @Component(
@@ -16,7 +17,7 @@ import org.osgi.service.component.annotations.Component;
 
 public class SendNotificationToUserHandler extends BaseUserNotificationHandler{
 	
-	public static final  String PORTLET_ID= AxHrmsLeaveManagementSystemWebPortletKeys.AXHRMS_MANAGER_LEAVE_MANAGEMENT_SYSTEM_WEB_PORTLET;
+	public static final  String PORTLET_ID= AxHrmsWorkFromHomePortletKeys.AXHRMSWORKFROMHOME;
 	
 	
     public SendNotificationToUserHandler() {
