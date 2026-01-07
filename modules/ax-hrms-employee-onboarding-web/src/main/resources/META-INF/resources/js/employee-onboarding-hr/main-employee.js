d49b646c4726a95ea6eac08a53f1d2113e1ee5fd
Keval Kathiriya <keval.kathiriya@aixtor.com>
2026-01-07 12:10:06 +0530

    validation changes in method revert.

--- modules/ax-hrms-employee-onboarding-web/src/main/resources/META-INF/resources/js/employee-onboarding-hr/main-employee.js
+++ modules/ax-hrms-employee-onboarding-web/src/main/resources/META-INF/resources/js/employee-onboarding-hr/main-employee.js
@@ -632,7 +632,7 @@ function setConfigsForAddExperienceSection(config) {
             });
             
 			$.validator.addMethod("validAddress", function (value) {
-                return /^[A-Za-z0-9\s,.-]+$/.test(value.trim());
+                return (value == '') || /^[A-Za-z0-9\s,.-]+$/.test(value.trim());
             });
 
             $.validator.addMethod("validCity", function (value) {
