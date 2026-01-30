8af0d41db5830d9e6cdfb10d46bd2bfe33bc5821
Ankit <ankit.hadiyal@aixtor.com>
2026-01-30 18:28:19 +0530

    Written Method for Add Comment while leave actions.

--- modules/ax-hrms/ax-hrms-service/src/main/java/com/ax/hrms/service/impl/CommentLocalServiceImpl.java
+++ modules/ax-hrms/ax-hrms-service/src/main/java/com/ax/hrms/service/impl/CommentLocalServiceImpl.java
@@ -9,8 +9,10 @@ import com.ax.hrms.model.Comment;
 import com.ax.hrms.service.base.CommentLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 
+import java.util.Date;
 import java.util.List;
 
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -45,5 +47,36 @@ public class CommentLocalServiceImpl extends CommentLocalServiceBaseImpl {
 	public List<Comment> findByTypeRequestIdAndStatus(long type, long requestId, boolean status) {
 		return commentPersistence.findByTypeRequestIdAndStatus(type, requestId, status);
 	}
-	
+	public Comment addWorkflowComment(
+			ThemeDisplay themeDisplay,
+			long type,
+			String action,
+			long requestId,
+			String commentText
+	) {
+
+		Comment comment =
+				commentPersistence.create(
+						counterLocalService.increment(Comment.class.getName())
+				);
+
+		Date now = new Date();
+
+		comment.setCompanyId(themeDisplay.getCompanyId());
+		comment.setGroupId(themeDisplay.getScopeGroupId());
+		comment.setCreatedBy(themeDisplay.getUserId());
+		comment.setModifiedBy(themeDisplay.getUserId());
+		comment.setCreateDate(now);
+		comment.setModifiedDate(now);
+
+		comment.setType(type);
+		comment.setAction(action);
+		comment.setComment(commentText);
+		comment.setRequestId(requestId);
+		comment.setStatus(true);
+
+		return commentPersistence.update(comment);
+	}
+
+
 }
