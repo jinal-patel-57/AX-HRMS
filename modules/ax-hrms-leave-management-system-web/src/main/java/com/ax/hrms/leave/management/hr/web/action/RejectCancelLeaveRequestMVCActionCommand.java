af06008cee04da16db8a897d94e3d4c7719b197d
pranipa <parth.ranipa@aixtor.com>
2026-02-04 14:55:12 +0530

    Fixed the issue of the comment status for leave module.

--- modules/ax-hrms-leave-management-system-web/src/main/java/com/ax/hrms/leave/management/hr/web/action/RejectCancelLeaveRequestMVCActionCommand.java
+++ modules/ax-hrms-leave-management-system-web/src/main/java/com/ax/hrms/leave/management/hr/web/action/RejectCancelLeaveRequestMVCActionCommand.java
@@ -32,158 +32,157 @@ import java.io.IOException;
 import java.time.LocalDate;
 
 /**
- * 
+ *
  * @author keval.kathiriya
  *
  */
 @Component(immediate = true, property = {
-		"javax.portlet.name=" + AxHrmsLeaveManagementSystemWebPortletKeys.AXHRMS_HR_LEAVE_MANAGEMENT_SYSTEM_WEB_PORTLET,
-		"mvc.command.name="
-				+ AxHrmsHrLeaveManagementSystemWebPortletConstants.REJECT_CANCEL_LEAVE_REQUEST }, service = MVCActionCommand.class)
+        "javax.portlet.name=" + AxHrmsLeaveManagementSystemWebPortletKeys.AXHRMS_HR_LEAVE_MANAGEMENT_SYSTEM_WEB_PORTLET,
+        "mvc.command.name="
+                + AxHrmsHrLeaveManagementSystemWebPortletConstants.REJECT_CANCEL_LEAVE_REQUEST}, service = MVCActionCommand.class)
 public class RejectCancelLeaveRequestMVCActionCommand extends BaseMVCActionCommand {
 
-	private static final Log log = LogFactoryUtil.getLog(RejectCancelLeaveRequestMVCActionCommand.class);
+    private static final Log log = LogFactoryUtil.getLog(RejectCancelLeaveRequestMVCActionCommand.class);
 
-	@Reference
-	LeaveTypeMasterLocalService leaveTypeMasterLocalService;
+    @Reference
+    LeaveTypeMasterLocalService leaveTypeMasterLocalService;
 
-	@Reference
-	NotificationTemplateConfiguration notificationTemplateConfiguration;
+    @Reference
+    NotificationTemplateConfiguration notificationTemplateConfiguration;
 
-	@Reference
-	LeaveRequestLocalService leaveRequestLocalService;
+    @Reference
+    LeaveRequestLocalService leaveRequestLocalService;
 
-	@Reference
-	LeaveCompensatoryStatusMasterLocalService leaveCompensatoryStatusMasterLocalService;
+    @Reference
+    LeaveCompensatoryStatusMasterLocalService leaveCompensatoryStatusMasterLocalService;
 
-	@Reference
-	LeaveBalanceLocalService leaveBalanceLocalService;
+    @Reference
+    LeaveBalanceLocalService leaveBalanceLocalService;
 
-	@Reference
-	LeaveDayTypeLocalService leaveDayTypeLocalService;
+    @Reference
+    LeaveDayTypeLocalService leaveDayTypeLocalService;
 
-	@Reference
-	AxHrmsCommonApi axHrmsCommonApi;
+    @Reference
+    AxHrmsCommonApi axHrmsCommonApi;
 
-	@Reference
-	EmployeeDetailsLocalService employeeDetailsLocalService;
+    @Reference
+    EmployeeDetailsLocalService employeeDetailsLocalService;
 
-	@Reference
-	DepartmentMasterLocalService departmentMasterLocalService;
+    @Reference
+    DepartmentMasterLocalService departmentMasterLocalService;
 
-	@Reference
-	DesignationMasterLocalService designationMasterLocalService;
+    @Reference
+    DesignationMasterLocalService designationMasterLocalService;
 
-	@Reference
-	EmployeeDepartmentLocalService employeeDepartmentLocalService;
+    @Reference
+    EmployeeDepartmentLocalService employeeDepartmentLocalService;
 
     @Reference
     CommentLocalService commentLocalService;
 
     @Reference
-	EmployeeDesignationLocalService employeeDesignationLocalService;
-	@Reference
-	AxHrmsHrLeaveRequestWebUtil leaveRequestWebUtil;
-	
-	@Reference
-	private MailTemplateConfiguration mailTemplateConfiguration;
+    EmployeeDesignationLocalService employeeDesignationLocalService;
+    @Reference
+    AxHrmsHrLeaveRequestWebUtil leaveRequestWebUtil;
 
-	@Override
-	protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortalException, IOException {
+    @Reference
+    private MailTemplateConfiguration mailTemplateConfiguration;
 
-		super.hideDefaultErrorMessage(actionRequest);
-		super.hideDefaultSuccessMessage(actionRequest);
-		ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+    @Override
+    protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortalException, IOException {
 
-		try {
+        super.hideDefaultErrorMessage(actionRequest);
+        super.hideDefaultSuccessMessage(actionRequest);
+        ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
 
-			Long leaveRequestId = ParamUtil.getLong(actionRequest,
-					AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_ID);
-			long cancelId = ParamUtil.getLong(actionRequest,
-					AxHrmsHrLeaveManagementSystemWebPortletConstants.CANCELLED_ID);
-			long rejectedId = ParamUtil.getLong(actionRequest,
-					AxHrmsHrLeaveManagementSystemWebPortletConstants.REJECTED_ID);
+        try {
+
+            Long leaveRequestId = ParamUtil.getLong(actionRequest,
+                    AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_ID);
+            long cancelId = ParamUtil.getLong(actionRequest,
+                    AxHrmsHrLeaveManagementSystemWebPortletConstants.CANCELLED_ID);
+            long rejectedId = ParamUtil.getLong(actionRequest,
+                    AxHrmsHrLeaveManagementSystemWebPortletConstants.REJECTED_ID);
             String comment = ParamUtil.getString(actionRequest, "comment");
-            log.info("comment is the :: "+comment);
+            log.info("comment is the :: " + comment);
             String fromName = PrefsPropsUtil.getString(themeDisplay.getCompanyId(), PropsKeys.ADMIN_EMAIL_FROM_NAME);
-			String fromEmailAddress = PrefsPropsUtil.getString(themeDisplay.getCompanyId(),
-					PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
+            String fromEmailAddress = PrefsPropsUtil.getString(themeDisplay.getCompanyId(),
+                    PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
 
-			LeaveRequest leaveRequest = leaveRequestLocalService.findByleaveRequestId(leaveRequestId);
-			EmployeeDetails employee = employeeDetailsLocalService.getEmployeeDetails(leaveRequest.getEmployeeId());
+            LeaveRequest leaveRequest = leaveRequestLocalService.findByleaveRequestId(leaveRequestId);
+            EmployeeDetails employee = employeeDetailsLocalService.getEmployeeDetails(leaveRequest.getEmployeeId());
 
-			long previousStatusId = leaveRequest.getLeaveCompensatoryStatusMasterId();
-			boolean isCancelled;
+            long previousStatusId = leaveRequest.getLeaveCompensatoryStatusMasterId();
+            boolean isCancelled;
 
-			if (Validator.isNotNull(cancelId)) {
-				isCancelled = true;
-				leaveRequest.setLeaveCompensatoryStatusMasterId(cancelId);
-				SessionMessages.add(actionRequest,
-						AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_CANCELLED);
-                commentLocalService.addWorkflowComment(themeDisplay, 1l, "Cancelled", leaveRequestId, comment);
+            if (Validator.isNotNull(cancelId)) {
+                isCancelled = true;
+                leaveRequest.setLeaveCompensatoryStatusMasterId(cancelId);
+                SessionMessages.add(actionRequest,
+                        AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_CANCELLED);
+                commentLocalService.addWorkflowComment(themeDisplay, 1l, "Cancel", leaveRequestId, comment);
 
             } else {
-				isCancelled = false;
-				leaveRequest.setLeaveCompensatoryStatusMasterId(rejectedId);
-				SessionMessages.add(actionRequest,
-						AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_REJECTED);
+                isCancelled = false;
+                leaveRequest.setLeaveCompensatoryStatusMasterId(rejectedId);
+                SessionMessages.add(actionRequest,
+                        AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_REJECTED);
                 commentLocalService.addWorkflowComment(themeDisplay, 1l, "Rejected", leaveRequestId, comment);
 
-			}
-			leaveRequest.setHrApprovalId(employeeDetailsLocalService.findByLrUserId(themeDisplay.getUserId()).getEmployeeId());
-			leaveRequest.setManagerApprovalId(employeeDetailsLocalService.findByLrUserId(themeDisplay.getUserId()).getEmployeeId());
-			leaveRequest.setModifiedBy(themeDisplay.getUserId());
-			leaveRequestLocalService.updateLeaveRequest(leaveRequest);
+            }
+            leaveRequest.setHrApprovalId(employeeDetailsLocalService.findByLrUserId(themeDisplay.getUserId()).getEmployeeId());
+            leaveRequest.setManagerApprovalId(employeeDetailsLocalService.findByLrUserId(themeDisplay.getUserId()).getEmployeeId());
+            leaveRequest.setModifiedBy(themeDisplay.getUserId());
+            leaveRequestLocalService.updateLeaveRequest(leaveRequest);
 
-			LeaveCompensatoryStatusMaster leaveStatusMasterOfRejected = leaveCompensatoryStatusMasterLocalService
-					.findByLeaveCompensatoryStatusName(AxHrmsHrLeaveManagementSystemWebPortletConstants.REJECTED);
+            LeaveCompensatoryStatusMaster leaveStatusMasterOfRejected = leaveCompensatoryStatusMasterLocalService
+                    .findByLeaveCompensatoryStatusName(AxHrmsHrLeaveManagementSystemWebPortletConstants.REJECTED);
 
-			if (previousStatusId != leaveStatusMasterOfRejected.getLeaveCompensatoryStatusMasterId()) {
+            if (previousStatusId != leaveStatusMasterOfRejected.getLeaveCompensatoryStatusMasterId()) {
 
-				LocalDate currentDate = LocalDate.now();
-				int currentYear = currentDate.getYear();
-				long employeeId = leaveRequest.getEmployeeId();
-				long leaveTypeMasterId = leaveRequest.getLeaveTypeMasterId();
-				double totalDays = 0;
+                LocalDate currentDate = LocalDate.now();
+                int currentYear = currentDate.getYear();
+                long employeeId = leaveRequest.getEmployeeId();
+                long leaveTypeMasterId = leaveRequest.getLeaveTypeMasterId();
+                double totalDays = 0;
 
-				// Counting Days on date wise .......
+                // Counting Days on date wise .......
 
-				totalDays = leaveRequestWebUtil.countLeaveDays(leaveRequestId, totalDays);
-				
-				// Update the Leave Balance
+                totalDays = leaveRequestWebUtil.countLeaveDays(leaveRequestId, totalDays);
 
-				leaveRequestWebUtil.updateLeaveBalance(currentYear, employeeId, leaveTypeMasterId, totalDays);
+                // Update the Leave Balance
 
-				// DECIDING SUBJECT  FOR CANCEL AND REJECT
-				String subject;
+                leaveRequestWebUtil.updateLeaveBalance(currentYear, employeeId, leaveTypeMasterId, totalDays);
 
-				if (leaveRequest.getLeaveCompensatoryStatusMasterId() == leaveStatusMasterOfRejected
-						.getLeaveCompensatoryStatusMasterId() ) {
-					subject = notificationTemplateConfiguration.leaveRejectedNotificationToEmployee();
-				} else {
-					subject =notificationTemplateConfiguration.leaveCancelledNotificationToEmployee();
-				}
+                // DECIDING SUBJECT  FOR CANCEL AND REJECT
+                String subject;
+
+                if (leaveRequest.getLeaveCompensatoryStatusMasterId() == leaveStatusMasterOfRejected
+                        .getLeaveCompensatoryStatusMasterId()) {
+                    subject = notificationTemplateConfiguration.leaveRejectedNotificationToEmployee();
+                } else {
+                    subject = notificationTemplateConfiguration.leaveCancelledNotificationToEmployee();
+                }
 
 
-				// SEND MAIL TO EMPLOYEE
+                // SEND MAIL TO EMPLOYEE
 
-				StringBuilder employeeMailBody = new StringBuilder(
-						AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_MAIL_HEAD);
+                StringBuilder employeeMailBody = new StringBuilder(
+                        AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_MAIL_HEAD);
 
-				leaveRequestWebUtil.sendMailtoEmployee(fromName, fromEmailAddress, leaveRequestId,
-						employeeMailBody,mailTemplateConfiguration,comment,false,isCancelled);
+                leaveRequestWebUtil.sendMailtoEmployee(fromName, fromEmailAddress, leaveRequestId,
+                        employeeMailBody, mailTemplateConfiguration, comment, false, isCancelled);
 
-				// SEND NOTITIFCATION TO EMPLOYEE
-				leaveRequestWebUtil.sendNotificationToEmployee(subject, employee);
+                // SEND NOTITIFCATION TO EMPLOYEE
+                leaveRequestWebUtil.sendNotificationToEmployee(subject, employee);
 
-			}
-		} catch (Exception e) {
-			log.error(" RejectCancelLeaveRequestMVCActionCommand >>> Action >>> " + e.getMessage());
-			SessionErrors.add(actionRequest, AxHrmsHrLeaveManagementSystemWebPortletConstants.SOME_ERROR_FOUND);
-		}
-		actionResponse.sendRedirect(PortalUtil.getLayoutFullURL(themeDisplay));
-	}
-
+            }
+        } catch (Exception e) {
+            log.error(" RejectCancelLeaveRequestMVCActionCommand >>> Action >>> " + e.getMessage());
+            SessionErrors.add(actionRequest, AxHrmsHrLeaveManagementSystemWebPortletConstants.SOME_ERROR_FOUND);
+        }
+        actionResponse.sendRedirect(PortalUtil.getLayoutFullURL(themeDisplay));
+    }
 
 
 }
--- modules/ax-hrms-leave-management-system-web/src/main/java/com/ax/hrms/leave/management/manager/web/action/RejectCancelLeaveRequestMVCActionCommand.java
+++ modules/ax-hrms-leave-management-system-web/src/main/java/com/ax/hrms/leave/management/manager/web/action/RejectCancelLeaveRequestMVCActionCommand.java
@@ -114,7 +114,7 @@ public class RejectCancelLeaveRequestMVCActionCommand extends BaseMVCActionComma
                 isCancelled = true;
                 leaveRequest.setLeaveCompensatoryStatusMasterId(cancelId);
                 SessionMessages.add(actionRequest, AxHrmsHrLeaveManagementSystemWebPortletConstants.LEAVE_REQUEST_CANCELLED);
-                commentLocalService.addWorkflowComment(themeDisplay, 1l, "Cancelled", leaveRequestId, comment);
+                commentLocalService.addWorkflowComment(themeDisplay, 1l, "Cancel", leaveRequestId, comment);
 
             } else {
                 isCancelled = false;
